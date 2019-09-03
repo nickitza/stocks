10e6ba87438186e434b85b8bcf8f515aabf8ff9a
Nicki Nixon <nickitza@gmail.com>
2019-09-03 17:08:52 -0600

    yarn react-app

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -1,16 +1,41 @@
 class Api::CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :update, :destroy]
+
   def index
+    render json: Company.all
   end
 
   def show
+    render json: @company
   end
 
   def create
+    company = Company.new(company_params)
+    if company.save
+      render json: company
+    else
+      render json: {errors: company.errors}, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @company.update(company_params)
+      render json: @company
+    else
+      render json: @company.errors, status: 422
+    end
   end
 
   def destroy
+    @company.destroy
   end
+
+  private
+    def company_params
+      params.require(:company).permit(:name)
+    end
+
+    def set_company
+      @company = Company.find(params[:id])
+    end
 end
