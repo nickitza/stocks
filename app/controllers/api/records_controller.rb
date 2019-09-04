3ab92366a3a714b97e3c8912749bdc2a2e446431
Nicki Nixon <nickitza@gmail.com>
2019-09-04 11:59:44 -0600

    records controller CRUD

--- app/controllers/api/records_controller.rb
+++ app/controllers/api/records_controller.rb
@@ -1,21 +1,34 @@
 class Api::RecordsController < ApplicationController
   before_action :set_company
-  before_action :set_record
+  before_action :set_record, only: [:show, :update, :destroy]
 
   def index
-    @records = @company.record
+    render json: @company.records
   end
 
   def show
+    render json: @record
   end
 
   def create
+    record = @company.record.new(record_params)
+    if record.save
+      render json: record
+    else
+      render json: record.errors, status: 422
+    end
   end
 
   def update
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: 422
+    end
   end
 
   def destroy
+    @record.destroy
   end
 
   private
