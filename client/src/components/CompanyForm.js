20d1049b323df65c96c3f4121fc78190f5848d9f
Nicki Nixon <nickitza@gmail.com>
2019-09-06 20:36:21 -0600

    company add

--- client/src/components/CompanyForm.js
+++ client/src/components/CompanyForm.js
@@ -1,15 +1,19 @@
 import React from "react"
 import {Form, } from "semantic-ui-react"
+import axios from 'axios'
 
 class CompanyForm extends React.Component {
   state = {name: "", ticker:""}
 
   handleSubmit = (e) => {
     e.preventDefault();
+    axios.post(`/api/companies`, {...this.state})
+    .then( res => this.setState({name:"", ticker:""}))
   }
 
-  handleChange = (e, {name, value}) => {
-    this.setState({ [name]: value})
+  handleChange = (e) => {
+    const { name, value, } = e.target;
+    this.setState({ [name]: value, });
   }
 
   render(){
